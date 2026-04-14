5bc8b16647d532253d9f747fa999920f4a97fbf0
ashutoshhota <ashutoshhota1729@gmail.com>
2026-04-14 10:39:57 +0530

    improved landing page

--- src/components/marketing/navbar.tsx
+++ src/components/marketing/navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export function Navbar() {
     return (
         <nav className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="container flex h-14 items-center">
+            <div className="container mx-auto flex h-14 items-center">
                 <div className="mr-4 hidden md:flex">
                     <Link href="/" className="mr-6 flex items-center space-x-2">
                         <span className="hidden font-bold sm:inline-block">Autonode</span>
